dbbd1c0b8408a259a809cfcd5af2e18b730b3003
[Naveen Kumar] <nk9411123@gmail.com>
2025-05-19 21:42:14 +0530

    String

---
+++ D19052025.js
@@ -0,0 +1,28 @@
+/** Date - 19/05/2025
+ * 3024. Type of Triangle
+ * 
+ * Problem statement:- You are given a 0-indexed integer array nums of size 3 which can form the sides of a triangle.
+ * A triangle is called equilateral if it has all sides of equal length
+ * A triangle is called isosceles if it has exactly two sides of equal length
+ * A triangle is called scalene if all its sides are of different lengths
+ * Return a string representing the type of triangle that can be formed or "none" if it cannot form a triangle.
+**/
+
+var triangleType = function(nums) {
+
+        nums.sort((a, b) => a - b);
+
+        
+    if (nums[0] + nums[1] <= nums[2]) {
+        return "none";
+    } else if (nums[0] === nums[2]) {
+        return "equilateral";
+    } else if (nums[0] === nums[1] || nums[1] === nums[2]) {
+        return "isosceles";
+    } else {
+        return "scalene";
+    }
+};
+
+let nums = [3,3,3];
+console.log(triangleType(nums));
